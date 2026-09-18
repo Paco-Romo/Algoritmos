b1fbe93d426288a3eb3b681442abf140e56c82ff
Jesus Fernando Smith Garcia <smithgarciajesusfernando310@gmail.com>
2026-09-18 10:33:52 -0700

    Se agrego el codigo de factorial recursivo e iterativo.

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,5 +1,31 @@
 public class Factorial {
-    //Todo Fer crea los algoritmos de factorial
-    //- Factorial - Iterativo (Práctica 4)
-    //- Factorial - Recursivo (Práctica 4)
-}
+    // Fer crea los algoritmos de factorial (Listo :P)
+
+    long factorial;
+    public class FactorialIterativo {
+        public long factorial(long n){
+            for(int i = 0; i<=n; i++){
+                if (factorial == 0){
+                    factorial = 1;
+                }
+                else {
+                    if (i == 0){
+                        factorial = 1;
+                    }
+                    factorial = factorial * i;
+
+                }
+            }
+            return factorial;
+        }
+    }
+
+    public class FactorialRecursivo {
+        public long numero(int n){
+            if(n<3)
+                return n;
+            else
+                return n * numero((n-1));
+        }
+    }
+    }
