ebd481fa1bf2f19ce9db70c503456073e85acdc0
Kevin_Moreno <kalejandro772@gmail.com>
2026-09-20 21:50:39 -0700

    67.6.7 - Finalizo mi parte

--- src/Enteros.java
+++ src/Enteros.java
@@ -1,6 +1,23 @@
 public class Enteros {
     //todo Kevin crea estos algoritmos
     //- Suma N enteros - Iterativo
+
+    public static int sumaIterativa(int n) {
+        int suma = 0;
+        for (int i = 1; i <= n; i++) {
+            suma = suma + i;
+        }
+        return suma;
+    }
+
     //- Suma N enteros - Recursivo
-    //- Fibonacci - Recursivo (con programación dinámica)
+
+    public static int sumaRecursiva(int n) {
+        if (n <= 0) {
+            return 0; // Caso base
+        }
+        return n + sumaRecursiva(n - 1);
+    }
+
+
 }
